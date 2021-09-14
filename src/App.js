3af57276ad0741fc76e3700dd53919051ebec815
Aquizi <47073178+Aquizi@users.noreply.github.com>
2021-09-13 21:28:17 -0400

    test1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Box2 from "./components/Box2";
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+        <Navbar className="Nav"/>
         <Box1/>
-        <Box2 data={{name:"Wes",  color:"Lime"}}/>
+        <Box2 data={{name:"Wesley",  color:"Lime"}}/>
         <Box2 data={{name:"Alex", color:"Black"}}/>
         <Box2 data={{name:"Max",  color:"Orange"}}/>
     </div>
